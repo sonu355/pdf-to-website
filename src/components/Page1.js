9ef1b23575173952566083dda1e1389e758aedc3
harshal <harshalc562@gmail.com>
2023-10-05 15:18:50 +0530

    starting with second task

---
+++ src/components/Page1.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+const Page1 = () => {
+  return (
+    <div className='container'>
+      <h1>Become a
+         Technology Partner
+      </h1>
+      img
+    </div>
+  )
+}
+
+export default Page1
