94f899287abf2416e2942c1f1f1ee8e2c3af89bf
harshal <harshalc562@gmail.com>
2023-10-06 15:44:22 +0530

    page number four is done

--- src/components/Page4.js
+++ src/components/Page4.js
@@ -1,24 +1,27 @@
 import React from 'react'
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
+import Button from 'react-bootstrap/Button'
 
+const code = require('../assets/code.png')
 const Page4 = () => {
   return (
     <div style={{ marginTop: "150px", marginLeft: "200px", marginBottom: '150px', marginRight:'200px' }}>
         <div className='d-flex' style={{backgroundColor:'#F8F8FF', padding:'80px 50px 80px 50px'}}>
             <Row>
                 <h1 style={{fontWeight:'bold'}}>Stop building from <br /> scratch!</h1>
-                <span style={{fontSize: '20px'}}> 
+                <span style={{fontSize: '16px'}}> 
                     Our developer-friendly APIs are
                     easy to understand,<br />implement & iterate.
                 </span>
-                <span style={{fontSize: '20px'}}>
+                <span style={{fontSize: '16px'}}>
                     Build on top of core features such as forms, 
                     shopping <br /> cart, user profile, wishlist & more.
                 </span>
+                <button style={{width:'250px', margin:'20px 0 0 12px'}}>Get Started</button>
             </Row>
             <Row>
-                img
+                <img src={code}></img>
             </Row>
         </div>
     </div>
