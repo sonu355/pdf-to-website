fcdefaf287656d4c8609f8b4b69ab08fc66ac2e1
harshal <harshalc562@gmail.com>
2023-10-06 15:03:59 +0530

    page number three is done

--- src/components/Page1.js
+++ src/components/Page1.js
@@ -7,8 +7,8 @@ const img1 = require("../assets/img1.jpeg");
 
 const Page1 = () => {
   return (
-    <div className="container" style={{ marginTop: "150px", marginLeft: "200px", marginBottom: '150px', marginRight:'200px' }}>
-        <Row className="d-flex container">
+    <div  style={{ marginTop: "150px", marginLeft: "200px", marginBottom: '150px', marginRight:'200px' }}>
+        <Row className="d-flex  mx-auto">
             <Col md={6} sm={12}>
                 <h1 style={{ fontWeight: "bolder" }}>
                     Become a <br />
@@ -38,18 +38,18 @@ const Page1 = () => {
                 <h2>Discover the simplicity of<br /> one platform that<br /> connects your buisness</h2>
                 <Button variant="dark" style={{ borderRadius:'none', marginTop:'10px'}}>See Use Cases</Button>
             </Col>
-            <Col sm={12} md={6} style={{ marginLeft:"-14vh"}}>
+            <Col sm={12} md={6} style={{ marginLeft:"-0vh"}}>
                 <Row >
                     <Col sm={4} md={4}><Button variant="none" style={{border:'0.5px solid grey', borderRadius:'30px'}}>Cross-Marketing</Button></Col>
-                    <Col sm={4} md={4}><Button variant="none" style={{border:'0.5px solid grey', borderRadius:'30px',marginLeft:'-6vh'}}>Rewards & incentives</Button></Col>
+                    <Col sm={4} md={4}><Button variant="none" style={{border:'0.5px solid grey', borderRadius:'30px',marginLeft:'0vh'}}>Rewards & incentives</Button></Col>
                 </Row>
                 <Row className="mt-2 mb-2">
                     <Col sm={6} md={6}><Button variant="none" style={{border:'0.5px solid grey', borderRadius:'30px'}}>CRM</Button></Col>
-                    <Col sm={6} md={6}><Button variant="none" style={{border:'0.5px solid grey', borderRadius:'30px',  marginLeft:"-30vh"}}>Buyer Journey Optimization</Button></Col>
+                    <Col sm={6} md={6}><Button variant="none" style={{border:'0.5px solid grey', borderRadius:'30px',  marginLeft:"-22vh"}}>Buyer Journey Optimization</Button></Col>
                 </Row>
                 <Row>
                     <Col sm={4} md={4}><Button variant="none" style={{border:'0.5px solid grey', borderRadius:'30px'}}>Data Collection</Button></Col>
-                    <Col sm={4} md={4}><Button variant="none" style={{border:'0.5px solid grey', borderRadius:'30px', marginLeft:"-8vh"}}>Social Tools</Button></Col>
+                    <Col sm={4} md={4}><Button variant="none" style={{border:'0.5px solid grey', borderRadius:'30px', marginLeft:"-1vh"}}>Social Tools</Button></Col>
                 </Row>
             </Col>
         </Row>    
--- src/components/Page3.js
+++ src/components/Page3.js
@@ -2,22 +2,77 @@ import React from 'react'
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 
+const img1 = require('../assets/com5img3.jpg')
+const img2 = require('../assets/com5img4.jpg')
+const img3 = require('../assets/com6img4.jpeg')
+const img4 = require('../assets/com7img5.jpeg')
+
 const Page3 = () => {
   return (
-    <div className="container" style={{ marginTop: "150px", marginLeft: "200px", marginBottom: '150px', marginRight:'200px' }}>
+    <div style={{ marginTop: "150px", marginLeft: "200px", marginBottom: '150px', marginRight:'200px' }}>
         <div>
             <h1 style={{ fontWeight: 'bolder'}}>Our tech, your growth </h1>
                 Monetize the most cutting-edge APIs in marketing
         </div>
-        <Row>
-            <Col>
-                Easy
+        <Row style={{marginTop:'40px'}}>
+            <Col className='d-flex'>
+                <div style={{ marginRight:'30px'}}>
+                    <img src={img1} alt='/' width='70px'></img>
+                </div>
+                <div>
+                    <h6 style={{ fontWeight:'bold'}}>
+                            Easy to integrate
+                    </h6>
+                    <span>
+                        Deeply integrate our APIs into your <br />
+                        applications with ease.
+                    </span>
+                </div>
             </Col>
-            <Col></Col>
+            <Col className='d-flex'>
+            <div style={{ marginRight:'30px'}}>
+                    <img src={img2} alt='/' width='70px'></img>
+                </div>
+                <div>
+                    <h6 style={{ fontWeight:'bold'}}>
+                        Data privacy first
+                    </h6>
+                    <span>
+                        Our in-built security protocols future-proof <br />
+                         you against data theft & ad fraud.
+                    </span>
+                </div> 
+            </Col>
         </Row>
-        <Row>
-            <Col></Col>
-            <Col></Col>
+        <Row style={{marginTop:'40px'}}>
+            <Col className='d-flex'>
+                <div style={{ marginRight:'30px'}}>
+                    <img src={img3} alt='/' width='70px'></img>
+                </div>
+                <div>
+                    <h6 style={{ fontWeight:'bold'}}>
+                        Open & transparent  
+                    </h6>
+                    <span>
+                        Our open APIs are easily accessible, so <br />
+                        you can tailor them to your needs.
+                    </span>
+                </div>
+            </Col>
+            <Col className='d-flex'>
+            <div style={{ marginRight:'30px'}}>
+                    <img src={img4} alt='/' width='70px'></img>
+                </div>
+                <div>
+                    <h6 style={{ fontWeight:'bold'}}>
+                        Universal standards
+                    </h6>
+                    <span>
+                        Our tech is built on Django and follows <br />
+                        familiar Open API standards.
+                    </span>
+                </div> 
+            </Col>
         </Row>
     </div>
   )
